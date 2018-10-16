4fd6373ae357e30717028833d77a9ffbe210d642
hena-singu <singu@hena.io>
2018-10-16 15:34:35 +0900

    setup react-navigator and add test code

---
+++ src/navigations/HomeNavigator.js
@@ -0,0 +1,15 @@
+import { createBottomTabNavigator } from 'react-navigation';
+
+import WalletScreen from '../screens/WalletScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+export default createBottomTabNavigator({
+    Wallet: {
+        screen: WalletScreen,
+        title: 'Wallet'
+    },
+    Settings: {
+        screen: SettingsScreen,
+        title: 'Settings'
+    }
+});
